40b84d5182cc01d3482e9e6d13c55ddf8a7a01c4
KyleMcIndoe <kmcindoe06@gmail.com>
2024-09-18 12:14:08 -0400

    Basic playability

--- Chess-Bot/dist/index.js
+++ Chess-Bot/dist/index.js
@@ -3,10 +3,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const chess_js_1 = require("chess.js");
+const bm_1 = require("./bm");
 const prompt_sync_1 = __importDefault(require("prompt-sync"));
 const prompt = (0, prompt_sync_1.default)();
-console.log("afef");
-var name = prompt("name:");
-console.log(name);
-//const b = new Chess();
+const b = new chess_js_1.Chess();
+const playerSide = prompt("User side: ");
+const depth = parseInt(prompt("Depth"));
+if (playerSide == 'w' || playerSide == 'White' || playerSide == 'white') {
+    console.log(b.ascii());
+    var firstMove = prompt("First move: ");
+    b.move(firstMove);
+}
+while (b.isGameOver() == false) {
+    var computerBm = bm_1.bm.findBest(b, depth);
+    b.move(computerBm);
+    console.log(b.ascii());
+    var userMove = prompt("Your move: ");
+    b.move(userMove);
+    console.log(b.ascii());
+}
 //# sourceMappingURL=index.js.map
--- Chess-Bot/src/index.ts
+++ Chess-Bot/src/index.ts
@@ -5,10 +5,28 @@ import PromptSync, * as promptSync from 'prompt-sync';
 
 const prompt = PromptSync();
 
-console.log("afef")
-var name = prompt("name:")
-console.log(name)
-//const b = new Chess();
+const b = new Chess();
+
+const playerSide = prompt("User side: ");
+const depth = parseInt(prompt("Depth"));
+
+if(playerSide == 'w' || playerSide == 'White' || playerSide == 'white') {
+    console.log(b.ascii());
+    var firstMove = prompt("First move: ");
+    b.move(firstMove);
+}
+
+while(b.isGameOver() == false) {
+    var computerBm = bm.findBest(b, depth);
+    b.move(computerBm);
+    console.log(b.ascii());
+
+    var userMove = prompt("Your move: ");
+    b.move(userMove);
+    console.log(b.ascii());
+}
+
+
 
 
 
