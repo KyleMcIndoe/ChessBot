db78a5939e104ca9124c6470ac035e61ff37d8b7
KyleMcIndoe <kmcindoe06@gmail.com>
2024-09-17 23:52:24 -0400

    Chess lib

--- Chess-Bot/dist/index.js
+++ Chess-Bot/dist/index.js
@@ -1,5 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const funcs_1 = require("./funcs");
+const chess_js_1 = require("chess.js");
+const c = new chess_js_1.Chess();
+console.log(c.ascii());
 funcs_1.funcs.s("things");
 //# sourceMappingURL=index.js.map
--- Chess-Bot/src/index.ts
+++ Chess-Bot/src/index.ts
@@ -1,3 +1,6 @@
 import {funcs} from './funcs';
+import {Chess} from 'chess.js';
 
+const c = new Chess();
+console.log(c.ascii());
 funcs.s("things");
